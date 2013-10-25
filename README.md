2bb8f94e093f2674d1c7a8be42e7c5a0a4b90376
Mister Hat <misterhat144@gmail.com>
2013-10-24 20:11:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# omdb
+A simple Node.JS module to access and normalize data from the
+[OMDb API](http://www.omdbapi.com/) by Bryan Fritz.
+
+## Installation
+    $ npm install omdb
+
+## Examples
+
+```javascript
+var omdb = require('omdb');
+
+omdb.search('saw', function(err, movies) {
+    if(err) {
+        return console.error(err);
+    }
+
+    if(movies.length < 1) {
+        return console.log('No movies were found!');
+    }
+
+    movies.forEach(function(movie) {
+        console.log('%s (%d)', movie.title, movie.year);
+    });
+
+    // Saw (2004)
+    // Saw II (2005)
+    // Saw III (2006)
+    // Saw IV (2007)
+    // ...
+});
+
+omdb.get({ title: 'Saw', year: 2004 }, true, function(err, movie) {
+    if(err) {
+        return console.error(err);
+    }
+
+    if(!movie) {
+        return console.log('Movie not found!');
+    }
+
+    console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
+    console.log(movie.plot);
+
+    // Saw (2004) 7.6/10
+    // Two men wake up at opposite sides of a dirty, disused bathroom, chained
+    // by their ankles to pipes. Between them lies...
+});
+```
+
+## API
+### .search(terms, callback)
+Run a search request on the API. `terms` should be a string containing search
+terms. `callback` returns an array of movies. If no movies are found, the array
+is empty.
+
+### .get(options, [fullPlot], callback)
+Run a single movie request on the API.
+
+`options` is assumed to be one of the following, respectively:
+
+1. An object with an `imdb` property.
+
+    `{ imdb: 'tt0387564' }`
+2. An object with a `title` property.
+
+    `{ title: 'Saw' }`
+3. An object with *both* a `title` and a `year` property.
+
+    `{ title: 'Saw', year: 2004 }`
+4. An IMDb ID string.
+
+    `'tt0387564'`
+5. A title string.
+
+    `'Saw'`
+
+`fullPlot` is an optional argument that if set to `true`, will attempt to
+request the extended version of the movie's plot.
+
+`callback` returns an object of the movie's information. If no movies are
+found, it will return `null`.
+
+## License
+Copyright (C) (2013) Mister Hat
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see [http://www.gnu.org/licenses/].
---
+++ index.js
@@ -0,0 +1,166 @@
+var request = require('request'),
+
+    host = 'http://www.omdbapi.com/';
+
+// Search for movies by titles.
+module.exports.search = function(terms, done) {
+    request({
+        url: host,
+        qs: { s: terms }
+    }, function(err, res, body) {
+        var movies;
+
+        if(err) {
+            return done(err);
+        }
+
+        if(res.statusCode !== 200) {
+            return done(new Error(res.statusCode));
+        }
+
+        try {
+            movies = JSON.parse(body);
+        } catch(e) {
+            return done(e);
+        }
+
+        // If no movies are found, the API returns
+        // "{"Response":"False","Error":"Movie not found!"}" instead of an
+        // empty array. So in this case, return an empty array to be consistent.
+        if(movies.Response === 'False') {
+            return done(null, []);
+        }
+
+        // Fix the ugly capitalized naming and cast the year as a Number.
+        done(null, movies.Search.map(function(movie) {
+            return {
+                title: movie.Title,
+                year: +movie.Year,
+                imdb: movie.imdbID,
+                type: movie.Type
+            };
+        }));
+    });
+};
+
+// Find a movie by title, title & year or IMDB ID. The second argument is
+// optional and determines whether or not to return an extended plot synopsis.
+module.exports.get = (function() {
+    var formatRuntime, formatVotes;
+
+    // Format strings of hours & minutes into minutes. For example,
+    // "1 h 30 min" == 90.
+    formatRuntime = function(runtime) {
+        var hours = runtime.match(/(\d+) h/),
+            minutes = runtime.match(/(\d+) min/);
+
+        hours = hours ? hours[1] : 0;
+        minutes = minutes ? +minutes[1] : 0;
+
+        return (hours * 60) + minutes;
+    };
+
+    // Strip foreign characters from the string and return a casted Number.
+    formatVotes = function(votes) {
+        return +votes.match(/\d/g).join('');
+    };
+
+    return function(options, fullPlot, done) {
+        var query = {};
+
+        // If the third argument is omitted, treat the second argument as the
+        // callback.
+        if(!done) {
+            done = fullPlot;
+            fullPlot = false;
+        }
+
+        query.plot = fullPlot ? 'full' : 'short';
+
+        // Select query based on explicit IMDB ID, explicit title, title & year,
+        // IMDB ID and title, respectively.
+        if(options.imdb) {
+            query.i = options.imdb;
+        } else if(options.title) {
+            query.t = options.title;
+
+            // In order to search with a year, a title must be present.
+            if(options.year) {
+                query.y = options.year;
+            }
+
+        // Assume anything beginning with "tt" and ending with digits is an
+        // IMDB ID.
+        } else if(/^tt\d+$/.test(options)) {
+            query.i = options;
+
+        // Finally, assume options is a string repesenting the title.
+        } else {
+            query.t = options;
+        }
+
+        request({ url: host, qs: query }, function(err, res, body) {
+            var movie;
+
+            if(err) {
+                return done(err);
+            }
+
+            if(res.statusCode !== 200) {
+                return done(new Error(res.statusCode));
+            }
+
+            try {
+                movie = JSON.parse(body);
+            } catch(e) {
+                return done(e);
+            }
+
+            // The movie being searched for could not be found.
+            if(movie.Response === 'False') {
+                return done(null, null);
+            }
+
+            // Replace 'N/A' strings with null for simple checks in the return
+            // value.
+            Object.keys(movie).forEach(function(key) {
+                if(movie[key] === 'N/A') {
+                    movie[key] = null;
+                }
+            });
+
+            // Beautify and normalize the ugly results the API returns.
+            done(null, {
+                title: movie.Title,
+                year: +movie.Year,
+                rated: movie.Rated,
+
+                // Cast the API's release date as a native JavaScript Date type.
+                released: movie.Released ? new Date(movie.Released) : null,
+
+                // Return runtime as minutes casted as a Number instead of an
+                // arbitrary string.
+                runtime: movie.Runtime ? formatRuntime(movie.Runtime) : null,
+
+                genres: movie.Genre ? movie.Genre.split(', ') : null,
+                director: movie.Director,
+                writers: movie.Writer ? movie.Writer.split(', ') : null,
+                plot: movie.Plot,
+
+                // A hotlink to a JPG of the movie poster on IMDB.
+                poster: movie.Poster,
+
+                imdb: {
+                    id: movie.imdbID,
+                    rating: movie.imdbRating ? +movie.imdbRating : null,
+
+                    // Convert votes from a US formatted string of a number to
+                    // a JavaScript Number.
+                    votes: movie.imdbVotes ? formatVotes(movie.imdbVotes) : null
+                },
+
+                type: movie.Type
+            });
+        });
+    };
+}());
